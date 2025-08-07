6498f37812ffc145f070d2d2517e5701f6fe6a08
Oli <oli@olillin.com>
2025-08-08 01:37:00 +0200

    Fix text validation

---
+++ tests/property/validateText.spec.ts
@@ -0,0 +1,67 @@
+import { validateText } from '../../src'
+
+it('allows lowercase letters', () => {
+    expect(() => {
+        validateText('abcdefghijklmnopqrstuvwxyz')
+    }).not.toThrow()
+})
+
+it('allows uppercase letters', () => {
+    expect(() => {
+        validateText('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    }).not.toThrow()
+})
+
+it('allows digits', () => {
+    expect(() => {
+        validateText('0123456789')
+    }).not.toThrow()
+})
+
+it('allows spaces', () => {
+    expect(() => {
+        validateText(' ')
+    }).not.toThrow()
+})
+
+it('allows :', () => {
+    expect(() => {
+        validateText(':')
+    }).not.toThrow()
+})
+
+it('allows "', () => {
+    expect(() => {
+        validateText(String.raw`\\`)
+    }).not.toThrow()
+})
+
+it('allows escaped \\n', () => {
+    expect(() => {
+        validateText(String.raw`\n`)
+    }).not.toThrow()
+})
+
+it('allows escaped \\,', () => {
+    expect(() => {
+        validateText(String.raw`\,`)
+    }).not.toThrow()
+})
+
+it('allows escaped \\\\', () => {
+    expect(() => {
+        validateText(String.raw`\\`)
+    }).not.toThrow()
+})
+
+it("doesn't allow unescaped ,", () => {
+    expect(() => {
+        validateText(',')
+    }).toThrow()
+})
+
+it("doesn't allow unescaped \\n", () => {
+    expect(() => {
+        validateText('\n')
+    }).toThrow()
+})
