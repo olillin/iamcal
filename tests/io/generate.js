9a3bc0ac63ec3aecc27edfcc7b19768dcf756f2d
Oli <oli@olillin.com>
2025-10-05 20:39:21 +0200

    Add file load runtime complexity test

--- tests/io/generate.js
+++ tests/io/generate.js
@@ -9,24 +9,44 @@ const {
     padZeros,
 } = require('../..')
 
-const calendar = new Calendar('abcd')
-for (let i = 0; i <= 999; i++) {
-    const stamp = new CalendarDateTime('20250708T120000')
-    const start =
-        i % 2 === 0
-            ? new CalendarDateTime('20250708T120000')
-            : new CalendarDate('20250708')
-    const end =
-        i % 2 === 0
-            ? new CalendarDateTime('20250708T130000')
-            : new CalendarDate('20250709')
-    const description = String.raw`Lorem ipsum dolor sit amet\, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam\, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident\, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+/**
+ * Generate a calendar containing a certain amount of events.
+ * @param {number} eventCount How many events the calendar should contain.
+ * @param {string} [name] An optional filename
+ * @returns {Promise<string>} The filename of the created calendar in the form "<eventCount>-events.ics".
+ */
+async function generateEvents(eventCount, name) {
+    const calendar = new Calendar('abcd')
+    for (let i = 0; i < eventCount; i++) {
+        const stamp = new CalendarDateTime('20250708T120000')
+        const start =
+            i % 2 === 0
+                ? new CalendarDateTime('20250708T120000')
+                : new CalendarDate('20250708')
+        const end =
+            i % 2 === 0
+                ? new CalendarDateTime('20250708T130000')
+                : new CalendarDate('20250709')
+        const description =
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-    const event = new CalendarEvent(padZeros(i, 3), stamp, start)
-        .setEnd(end)
-        .setSummary(`Event ${i}`)
-        .setDescription(description)
-        .setLocation('Lorem ipsum')
-    calendar.addComponent(event)
+        const event = new CalendarEvent(
+            padZeros(i, String(eventCount - 1).length),
+            stamp,
+            start
+        )
+            .setEnd(end)
+            .setSummary(`Event ${i}`)
+            .setDescription(description)
+            .setLocation('Lorem ipsum')
+        calendar.addComponent(event)
+    }
+    const filename = name ?? `${eventCount}-events.ics`
+    await dump(calendar, filename)
+    return filename
 }
-dump(calendar, '1000-events.ics')
+
+generateEvents(1_000)
+generateEvents(10_000, '10-000-events.ics')
+generateEvents(20_000, '20-000-events.ics')
+generateEvents(40_000, '40-000-events.ics')
--- tests/io/load.spec.ts
+++ tests/io/load.spec.ts
@@ -6,9 +6,31 @@ it('can load a calendar file successfully', () => {
     }).not.toThrow()
 })
 
-it('takes less than 500ms to load 1000 events', async () => {
+it('takes less than 5,000ms to load 1,000 events', async () => {
     const start = performance.now()
     await load(__dirname + '/1000-events.ics')
     const end = performance.now()
     expect(end - start).toBeLessThan(500)
-})
+}, 6_000)
+
+it('does not have a complexity worse than linear', async () => {
+    const start = performance.now()
+    await load(__dirname + '/10-000-events.ics')
+    const end1 = performance.now()
+    await load(__dirname + '/20-000-events.ics')
+    const end2 = performance.now()
+    await load(__dirname + '/40-000-events.ics')
+    const end3 = performance.now()
+
+    const time1 = end1 - start
+    const time2 = end2 - end1
+    const time3 = end3 - end2
+
+    console.log(`Took ${time1.toFixed(0)} ms to parse 10,000 events`)
+    console.log(`Took ${time2.toFixed(0)} ms to parse 20,000 events`)
+    console.log(`Took ${time3.toFixed(0)} ms to parse 40,000 events`)
+
+    expect(time2 / time1).toBeCloseTo(2, 1)
+    expect(time3 / time2).toBeCloseTo(2, 1)
+    expect(time3 / time1).toBeCloseTo(4, 1)
+}, 100_000)
